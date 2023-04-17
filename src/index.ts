6d37c02bd51a8239ce4e7b9aa9d12f93e397a1e6
takoyaki9n <wtr816@gmail.com>
2023-04-17 22:31:36 +0900

    Implement parse

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as readline from 'readline/promises';
 import { stdin, stdout } from 'process';
 import lex from './lex';
+import parse from './parse';
 
 const main = async () => {
     const rl = readline.createInterface({ input: stdin, output: stdout });
@@ -13,9 +14,13 @@ const main = async () => {
             break;
         }
 
-        const tokens = lex(program);
-
-        console.log(tokens);
+        try {
+            const tokens = lex(program);       
+            const expr = parse(tokens);  
+            console.log("%j", expr);
+        } catch (error) {
+            console.error(error);            
+        }
     }
 
     rl.close();
---
+++ src/parse.ts
@@ -0,0 +1,41 @@
+type SExpr = null | string | [SExpr, SExpr];
+
+const parseList = (tokens: string[]): SExpr => {
+    const token = tokens[0];
+    switch (token) {
+        case undefined:
+            throw new Error("Unclosed open paren");
+        case ")":
+            tokens.shift();
+            return null;
+        default:
+            const car = parseSExpression(tokens);
+            const cdr = parseList(tokens);
+            return [car, cdr];
+    }
+};
+
+const parseSExpression = (tokens: string[]): SExpr => {
+    const token = tokens.shift();
+    switch (token) {
+        case undefined:
+            throw new Error("Unexpected EOF");
+        case "(":
+            return parseList(tokens);
+        case ")":
+            throw new Error("Unexpected token: )");
+        default:
+            return token;
+    }
+};
+
+const parse = (tokens: string[]): SExpr => {
+    let expr = parseSExpression(tokens);
+    if (tokens.length == 0) {
+        return expr;
+    } else {
+        throw new Error("Redundant expression");
+    }
+};
+
+export default parse;
