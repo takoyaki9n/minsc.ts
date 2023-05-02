16be8d1a7357e4ae4b8532e68032e23f94f345ec
takoyaki9n <wtr816@gmail.com>
2023-05-02 19:09:04 +0900

    Add tests for evaluate

--- src/eval.test.ts
+++ src/eval.test.ts
@@ -18,4 +18,34 @@ describe("evaluate", () => {
         const actual = evaluate(expr);
         expect(actual).toEqual(expected);
     });
+
+    test.each([
+        ["(if (< 1 2) 3 4)", number(3)],
+        ["(if (> 1 2) 3 4)", number(4)],
+    ])("if form %p", (program, expected) => {
+        const expr = parse(lex(program));
+        const actual = evaluate(expr);
+        expect(actual).toEqual(expected);
+    });
+
+    test("lambda expression", () => {
+        const program = "((lambda (x y) (+ (* x x) (* y y))) 3 4)";
+        const expr = parse(lex(program));
+        const actual = evaluate(expr);
+        expect(actual).toEqual(number(25));
+    });
+
+    test("let expression", () => {
+        const program = 
+        `(let ((fix (lambda (f)
+                    ((lambda (x) (f (lambda (y) ((x x) y))))
+                        (lambda (x) (f (lambda (y) ((x x) y)))))))
+               (fact (lambda (f)
+                    (lambda (n)
+                        (if (< n 2) 1 (* n (f (- n 1))))))))
+                ((fix fact) 4))`;
+        const expr = parse(lex(program));
+        const actual = evaluate(expr);
+        expect(actual).toEqual(number(24));
+    });
 });
